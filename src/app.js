8043c274ca4c6cc09b8c6a0e86d7e861958c86d8
Javier Morales <jmoralesm45@miumg.edu.gt>
2023-09-06 16:14:23 -0600

    Se agregaron variables de entorno

---
+++ src/app.js
@@ -0,0 +1,18 @@
+import express from "express";
+import personalRoutes from './routes/Personal.routes.js';
+import indexRoutes from './routes/index.routes.js';
+
+const app = express()
+
+app.use(express.json())
+
+app.use(indexRoutes)
+app.use('/api',personalRoutes)
+
+app.use ((req, res, next) => {
+    res.status(404).json({
+        message: 'EndPoint no encontrado'
+    })
+})
+
+export default app;
--- src/index.js
+++ src/index.js
@@ -1,19 +1,4 @@
-import express from "express";
-import personalRoutes from './routes/Personal.routes.js';
-import indexRoutes from './routes/index.routes.js';
-
-const app = express()
-
-app.use(express.json())
-
-app.use(indexRoutes)
-app.use('/api',personalRoutes)
-
-app.use ((req, res, next) => {
-    res.status(404).json({
-        message: 'EndPoint no encontrado'
-    })
-})
-
-app.listen(3000)
-console.log('Server running on port 3000')
+import app from './app.js'
+import {PORT} from './config.js'
+app.listen(PORT)
+console.log('Server running on port ', PORT)
