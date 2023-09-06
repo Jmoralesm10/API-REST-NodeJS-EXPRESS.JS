5ac751f2e3da46c5f11c302ee372253b37a5bcce
Javier Morales <jmoralesm45@miumg.edu.gt>
2023-09-06 15:35:32 -0600

    Se agregaro el manejo en caso de que ingresen una url invalida

--- src/controllers/personal.controller.js
+++ src/controllers/personal.controller.js
@@ -23,39 +23,39 @@ export const getpersona = async (req, res) => {
 
 //Crear empleado
 export const postpersonal = async (req, res) => {
-    try{
-    const { Nombres, Apellidos, Direccion, Telefono, Puesto, Fecha_Contratacion, Salario } = req.body
-    const [rows] = await pool.query('INSERT INTO personal (Nombres,Apellidos,Direccion,Telefono,Puesto,fecha_contratacion,Salario) values (?,?,?,?,?,?,?)', [Nombres, Apellidos, Direccion, Telefono, Puesto, Fecha_Contratacion, Salario])
-    res.send({
-        id: rows.insertId,
-        Nombres,
-        Apellidos,
-        Direccion,
-        Telefono,
-        Puesto,
-        Fecha_Contratacion,
-        Salario
-    })
-    } catch (error){
+    try {
+        const { Nombres, Apellidos, Direccion, Telefono, Puesto, Fecha_Contratacion, Salario } = req.body
+        const [rows] = await pool.query('INSERT INTO personal (Nombres,Apellidos,Direccion,Telefono,Puesto,fecha_contratacion,Salario) values (?,?,?,?,?,?,?)', [Nombres, Apellidos, Direccion, Telefono, Puesto, Fecha_Contratacion, Salario])
+        res.send({
+            id: rows.insertId,
+            Nombres,
+            Apellidos,
+            Direccion,
+            Telefono,
+            Puesto,
+            Fecha_Contratacion,
+            Salario
+        })
+    } catch (error) {
         return res.status(500).json({ Message: 'Algo salio mal!' })
-    }    
+    }
 };
 
 //Actualizar empleado
 export const putpersonal = async (req, res) => {
-    try{
-    const { id } = req.params
-    const { Nombres, Apellidos, Direccion, Telefono, Puesto, Salario } = req.body
-    const [result] = await pool.query('UPDATE personal SET Nombres = ?, Apellidos = ?, Direccion = ?, Telefono = ?, Puesto = ?, Salario = ? WHERE id_personal = ?', [Nombres, Apellidos, Direccion, Telefono, Puesto, Salario, id])
+    try {
+        const { id } = req.params
+        const { Nombres, Apellidos, Direccion, Telefono, Puesto, Salario } = req.body
+        const [result] = await pool.query('UPDATE personal SET Nombres = ?, Apellidos = ?, Direccion = ?, Telefono = ?, Puesto = ?, Salario = ? WHERE id_personal = ?', [Nombres, Apellidos, Direccion, Telefono, Puesto, Salario, id])
 
-    if (result.affectedRows === 0) return res.status(404).json({
-        message: 'Registro no actualizado'
-    })
+        if (result.affectedRows === 0) return res.status(404).json({
+            message: 'Registro no actualizado'
+        })
 
-    const [rows] = await pool.query('SELECT * FROM personal where id_personal = ?', [req.params.id])
-    res.send(rows[0])
+        const [rows] = await pool.query('SELECT * FROM personal where id_personal = ?', [req.params.id])
+        res.send(rows[0])
     }
-    catch(error){
+    catch (error) {
         return res.status(500).json({ Message: 'Algo salio mal!' })
     }
 
@@ -63,18 +63,18 @@ export const putpersonal = async (req, res) => {
 
 //Despedir empleado
 export const patchpersonal = async (req, res) => {
-    try{
-    const { id } = req.params
-    const { Fecha_despido } = req.body
-    const [result] = await pool.query('UPDATE personal SET Fecha_despido = ? WHERE id_personal = ?', [Fecha_despido, id])
+    try {
+        const { id } = req.params
+        const { Fecha_despido } = req.body
+        const [result] = await pool.query('UPDATE personal SET Fecha_despido = ? WHERE id_personal = ?', [Fecha_despido, id])
 
-    if (result.affectedRows === 0) return res.status(404).json({
-        message: 'Registro no actualizado'
-    })
+        if (result.affectedRows === 0) return res.status(404).json({
+            message: 'Registro no actualizado'
+        })
 
-    const [rows] = await pool.query('SELECT * FROM personal where id_personal = ?', [req.params.id])
-    res.send(rows[0])
-    } catch(error){
+        const [rows] = await pool.query('SELECT * FROM personal where id_personal = ?', [req.params.id])
+        res.send(rows[0])
+    } catch (error) {
         return res.status(500).json({ Message: 'Algo salio mal!' })
     }
 
@@ -83,15 +83,15 @@ export const patchpersonal = async (req, res) => {
 //Eliminar Empleado
 export const deletetpersonal = async (req, res) => {
 
-    try{
-    const [result] = await pool.query('DELETE FROM personal where id_personal = ?', [req.params.id])
+    try {
+        const [result] = await pool.query('DELETE FROM personal where id_personal = ?', [req.params.id])
 
-    if (result.affectedRows <= 0) return res.status(404).json({
-        message: 'El empleado no existe'
-    })
+        if (result.affectedRows <= 0) return res.status(404).json({
+            message: 'El empleado no existe'
+        })
 
-    res.sendStatus(204)
-    } catch (error){
+        res.sendStatus(204)
+    } catch (error) {
         return res.status(500).json({ Message: 'Algo salio mal!' })
     }
 }
--- src/index.js
+++ src/index.js
@@ -9,5 +9,11 @@ app.use(express.json())
 app.use(indexRoutes)
 app.use('/api',personalRoutes)
 
+app.use ((req, res, next) => {
+    res.status(404).json({
+        message: 'EndPoint no encontrado'
+    })
+})
+
 app.listen(3000)
 console.log('Server running on port 3000')
